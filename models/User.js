deab72a408ded32a93fb1000a5484bea90e95d72
janhvibhatt <janhvibhatt112@gmail.com>
2025-04-14 20:11:50 +0530

    changes

---
+++ models/User.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    _id: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    imageUrl: {
+        type:String,
+        required: true,
+    },
+    cartItems : {
+        type: Object,
+        default: {},
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+}, {
+    minimize: false
+})
+
+const User = mongoose.models.user || mongoose.model('User', userSchema)
+
+export default User
