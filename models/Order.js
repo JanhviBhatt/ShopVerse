37137fabc76b330597cb34953d3cc2b02ea5a9a4
janhvibhatt <janhvibhatt112@gmail.com>
2025-04-18 13:26:55 +0530

    changes

--- models/Order.js
+++ models/Order.js
@@ -1,40 +1,42 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema({
-    userId:{
-        type:String,
-        required:true,
-        ref:'user'
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User", // Make sure this matches the model name of your user schema (case-sensitive)
+  },
+  items: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "Product", // Ensure this matches your Product model name
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
     },
-    items:[{
-        product:{
-            type:String,
-            required:true,
-            ref:'product'
-        },
-        quantity:{
-            type:Number,
-            required:true
-        },
-    }],
-    amount:{
-        type:Number,
-        required:true
-    },
-    address:{
-        type:String,
-        ref:'address',
-        required:true
-    },
-    status:{
-        type:String,
-        required:true,
-        default:"Order Placed"
-    },
-    date:{
-        type:Number,
-        required:true,
-    },
-})
-const Order = mongoose.models.order || mongoose.model("order",orderSchema)
-export default Order
+  ],
+  amount: {
+    type: Number,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    default: "Order Placed",
+  },
+  date: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Order = mongoose.models.order || mongoose.model("order", orderSchema);
+export default Order;
